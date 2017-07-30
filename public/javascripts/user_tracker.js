e5fb3e21ecf2a7e3e7022c41b8098e1a5cecde73
Felipe Alberto Treichel <felipealbertot@gmail.com>
2017-07-30 19:20:09 -0300

    alteração de localstorage para sessionstorage

--- public/javascripts/user_tracker.js
+++ public/javascripts/user_tracker.js
@@ -4,13 +4,13 @@ var userTrackerURL = "https://serene-temple-33600.herokuapp.com/api/v1/users";
 function sendVisitedPagesToUserTrackerApplication() {
   var user_name = "";
   var user_email = "";
-  if(localStorage.getItem('user')){
-    var user = JSON.parse(localStorage.getItem('user'));
+  if(sessionStorage.getItem('user')){
+    var user = JSON.parse(sessionStorage.getItem('user'));
     user_name = user.name;
     user_email = user.email;
   }
 
-  var user = {user:{name: user_name, email: user_email, pages_attributes: JSON.parse(localStorage.getItem('visited_pages'))}};
+  var user = {user:{name: user_name, email: user_email, pages_attributes: JSON.parse(sessionStorage.getItem('visited_pages'))}};
   
   $.ajax({
       type: "POST",
@@ -18,7 +18,7 @@ function sendVisitedPagesToUserTrackerApplication() {
       url: userTrackerURL,
       contentType: "application/json"
   }).done(function(data) {
-    localStorage.setItem('user', JSON.stringify(data));
+    sessionStorage.setItem('user', JSON.stringify(data));
   });
 }
 
@@ -26,7 +26,7 @@ beforeSubmit = function(){
   var user_name = document.forms["contactForm"]["name"].value;
   var user_email = document.forms["contactForm"]["email"].value;
 
-  var user = {user:{name: user_name, email: user_email, pages_attributes: JSON.parse(localStorage.getItem('visited_pages'))}};
+  var user = {user:{name: user_name, email: user_email, pages_attributes: JSON.parse(sessionStorage.getItem('visited_pages'))}};
   
   $.ajax({ 
      type: "POST",
@@ -34,7 +34,7 @@ beforeSubmit = function(){
      url: userTrackerURL,
      contentType: "application/json",
      success: function(data) {
-       localStorage.setItem('user', JSON.stringify(data));
+       sessionStorage.setItem('user', JSON.stringify(data));
      },
      complete: function() { 
        $('#contactForm').submit();
@@ -46,17 +46,17 @@ function savePageToLocalStorage() {
   var page = {name: document.title, url: document.URL, visited_at: new Date()}
   console.log(page);
   var visitedPages = [];
-  if(localStorage.getItem('visited_pages')){
-    visitedPages = JSON.parse(localStorage.getItem('visited_pages'));
+  if(sessionStorage.getItem('visited_pages')){
+    visitedPages = JSON.parse(sessionStorage.getItem('visited_pages'));
     var exists = visitedPages.find(p => p.name === page.name);
     if(exists)
       return false;
   }
   visitedPages.push(page);
-  localStorage.setItem('visited_pages', JSON.stringify(visitedPages));
+  sessionStorage.setItem('visited_pages', JSON.stringify(visitedPages));
   
-  if(localStorage.getItem('user')){
-    var user = JSON.parse(localStorage.getItem('user'));
+  if(sessionStorage.getItem('user')){
+    var user = JSON.parse(sessionStorage.getItem('user'));
     if(user.id){
       sendVisitedPagesToUserTrackerApplication();
     }
